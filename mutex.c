9a8e280a09a5176c3ec4ff2b56fc6170d827aa01
Zichuan Tian <ztian29@wisc.edu>
2019-01-25 03:50:25 -0600

    added mutex.c

--- mutex.c
+++ mutex.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <sys/time.h>
 
-pthread_spinlock_t lock;
-int pshared = PTHREAD_PROCESS_PRIVATE;
+pthread_mutex_t m;
 volatile int counter = 0; 
 int loops;
 
 void *worker(void *arg) {
     int i;
     for (i = 0; i < loops; i++) {
-      pthread_spin_lock(&lock);
+      pthread_mutex_lock(&m);
       ++counter;
-      pthread_spin_unlock(&lock);
+      pthread_mutex_unlock(&m);
     }
     pthread_exit(NULL);
 }
@@ -22,19 +20,18 @@ int
 main(int argc, char *argv[])
 {
     struct timeval tv1, tv2;
-    if (argc != 2) {
+    if (argc != 2) { 
       exit(1); 
     } 
     loops = atoi(argv[1]);
     pthread_t p1, p2;
-    pthread_spin_init(&lock, pshared);
+    pthread_mutex_init(&m, NULL);
     gettimeofday(&tv1, NULL);
     pthread_create(&p1, NULL, worker, NULL); 
     pthread_create(&p2, NULL, worker, NULL);
     pthread_join(p1, NULL);
     pthread_join(p2, NULL);
-    printf("result value   : %d\n", counter);
-    pthread_spin_destroy(&lock);
+    printf("Final value   : %d\n", counter);
     gettimeofday(&tv2, NULL);
     if (tv1.tv_usec > tv2.tv_usec){
       tv2.tv_sec--;
