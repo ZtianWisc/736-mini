c545c739bd8da2d2ff638bbee4ebb9d948034d2a
Zichuan Tian <ztian29@wisc.edu>
2019-01-26 21:18:26 -0600

    test direct io

---
+++ buf.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    FILE *rd;
+    FILE *wt;
+    size_t io_bufsize;
+    char *io_buffer;
+
+    f = fopen("read", "wb");
+    if (rd == NULL)
+    {
+        perror("fopen failed\n");
+        return -1;
+    }
+
+    io_bufsize = __fbufsize(rd);
+    printf("The buffer size is %zd\n", io_bufsize);
+    if (strcmp(argv[1], "-d")==0){
+        setvbuf(rd, NULL, _IONBF, 0);
+    } else {
+        buffer = (char*) malloc(atoi(argv[1]) * sizeof(char));
+        setvbuf(rd, io_buffer, _IOFBF, atoi(argv[1]));
+    }
+    io_bufsize = __fbufsize(rd);
+    printf("The buffer size is %zd\n", io_bufsize);
+    exit(0);
+}
