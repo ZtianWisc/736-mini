e09206199a4b6ad04a514605677393e00ae06fc4
Zichuan Tian <ztian29@wisc.edu>
2019-01-26 23:32:31 -0600

    fixed write buffer

--- buf.c
+++ buf.c
@@ -19,20 +19,24 @@ int main(int argc, char* argv[])
     }
     if (strcmp(argv[1], "-d")==0){
         setvbuf(rd, NULL, _IONBF, 0);
+        setvbuf(wt, NULL, _IONBF, 0);
     } else {
         io_buffer = (char*) malloc(atoi(argv[1]) * sizeof(char));
         setvbuf(rd, io_buffer, _IOFBF, atoi(argv[1]));
+        setvbuf(wt, io_buffer, _IOFBF, atoi(argv[1]));
     }
     io_bufsize = __fbufsize(rd);
-    printf("The buffer size is %zd\n", io_bufsize);
+    printf("The I/O buffer size is %zd\n", io_bufsize);
 
     // write from read to write
     size_t n_char = atoi(argv[2]);
     char* buffer = (char*) malloc(n_char * sizeof(char));
+    long double bytes = 0.0;
     // get start time
     gettimeofday(&tv1, NULL);
     while(fread(buffer, sizeof(char), n_char, rd) != 0){
         fwrite(buffer, sizeof(char), n_char, wt);
+        byte += n_char;
     }
     // get done time
     gettimeofday(&tv2, NULL);
@@ -42,5 +46,10 @@ int main(int argc, char* argv[])
     }
     printf("Time cost:   %ld.%ld\n", tv2.tv_sec - tv1.tv_sec, 
             tv2.tv_usec - tv1.tv_usec);
+    printf("Throughput: %lf MB/s\n", 
+            byte/(1024 * 1024)/(tv2.tv_sec-tv1.tv_sec 
+            + (tv2.tv_usec-tv1.tv_usec)/1000000));
+    fclose(rd);
+    fclose(wt);
     exit(0);
 }
